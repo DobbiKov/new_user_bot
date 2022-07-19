b7999988f14ff616a0c34564f4c397776af374e2
DobbiKov <dobbikov@gmail.com>
2022-07-19 23:28:03 +0200

    We just specified we made better detection of language.

--- handlers/handle_voice.py
+++ handlers/handle_voice.py
@@ -1,10 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram import types
-from modules.translate_voice import delete_files, translate_voice, convert_ogg_wav, translate_voice_texts
+from modules.translate_voice import delete_files, translate_voice, convert_ogg_wav, translate_voice_texts, translate_voice_text
 
-async def get_voice(client, message: types.Message):
+async def get_voice(client: Client, message: types.Message):
     if message.chat.id < 0 and message.chat.id != -1001711218638:
         return False
+    new_mess = await client.send_message(message.chat.id, "Please, wait...")
     # await client.send_message(message.chat.id, "о, повідомлення голосове")
     print(message.voice.file_id)
     file = await client.download_media(message.voice.file_id)
@@ -12,16 +13,51 @@ async def get_voice(client, message: types.Message):
     file_name = file #/Users/dsadas/
     # # file_bytes = bytes(file.getbuffer())
     file_path = convert_ogg_wav(file_name)
-    text_fr, text_ua = translate_voice(file_path)
+    text, lang = translate_voice(file_path)
 
-    translated_text_fr, translated_text_ua = translate_voice_texts(text_fr, text_ua)
+    if lang == None:
+        try:
+            await client.edit_message_text(new_mess.chat.id, new_mess.id, "I haven't understood your message. Try again please")
+        except:
+            pass
+        delete_files(file_path)
+        return
+
+    translated_text = translate_voice_text(text, lang)
 
     text_message = f"\
 {message.from_user.first_name}:\n\n\
-{text_fr}\n\
-{text_ua}\n\n\
+{text}\n\n\
 Translation:\n\
-{translated_text_fr}\n\
-{translated_text_ua}"
-    await client.send_message(message.chat.id, text_message)
+{translated_text}"
+    # await client.send_message(message.chat.id, text_message)
+    try:
+        await client.edit_message_text(new_mess.chat.id, new_mess.id, text_message)
+    except:
+        pass
     delete_files(file_path)
+
+
+# async def get_voice(client, message: types.Message):
+#     if message.chat.id < 0 and message.chat.id != -1001711218638:
+#         return False
+#     # await client.send_message(message.chat.id, "о, повідомлення голосове")
+#     print(message.voice.file_id)
+#     file = await client.download_media(message.voice.file_id)
+
+#     file_name = file #/Users/dsadas/
+#     # # file_bytes = bytes(file.getbuffer())
+#     file_path = convert_ogg_wav(file_name)
+#     text_fr, text_ua = translate_voice(file_path)
+
+#     translated_text_fr, translated_text_ua = translate_voice_texts(text_fr, text_ua)
+
+#     text_message = f"\
+# {message.from_user.first_name}:\n\n\
+# {text_fr}\n\
+# {text_ua}\n\n\
+# Translation:\n\
+# {translated_text_fr}\n\
+# {translated_text_ua}"
+#     await client.send_message(message.chat.id, text_message)
+#     delete_files(file_path)
--- modules/translate_voice.py
+++ modules/translate_voice.py
@@ -6,6 +6,37 @@ import os
 
 r = sr.Recognizer()
 
+def translate_voice_bilingual(file_name):
+    with sr.AudioFile(file_name) as source:
+        # listen for the data (load audio to memory)
+        audio_data = r.record(source)
+        # recognize (convert from speech to text)
+        text_fr = ""
+        text_ua = ""
+
+        try:
+            text_fr = r.recognize_google(audio_data, language="fr-FR", show_all=True)
+            text_ua = r.recognize_google(audio_data, language="uk-UA", show_all=True)
+            french_text = french_confidence = ukrainian_text = ukrainian_confidence = None
+            try:
+                french_text = text_fr["alternative"][0]["transcript"]
+                french_confidence = text_fr["alternative"][0]["confidence"]
+            except: 
+                french_text = ""
+                french_confidence = 0
+
+            try:
+                ukrainian_text = text_ua["alternative"][0]["transcript"]
+                ukrainian_confidence = text_ua["alternative"][0]["confidence"]
+            except:
+                ukrainian_text = ""
+                ukrainian_confidence = 0
+
+            
+        except: 
+            pass
+
+        return text_fr, text_ua
 def translate_voice(file_name):
     with sr.AudioFile(file_name) as source:
         # listen for the data (load audio to memory)
@@ -15,12 +46,32 @@ def translate_voice(file_name):
         text_ua = ""
 
         try:
-            text_fr = r.recognize_google(audio_data, language="fr-FR")
-            text_ua = r.recognize_google(audio_data, language="uk-UA")
+            text_fr = r.recognize_google(audio_data, language="fr-FR", show_all=True)
+            text_ua = r.recognize_google(audio_data, language="uk-UA", show_all=True)
+            french_text = french_confidence = ukrainian_text = ukrainian_confidence = None
+            try:
+                french_text = text_fr["alternative"][0]["transcript"]
+                french_confidence = text_fr["alternative"][0]["confidence"]
+            except: 
+                french_text = ""
+                french_confidence = 0
+
+            try:
+                ukrainian_text = text_ua["alternative"][0]["transcript"]
+                ukrainian_confidence = text_ua["alternative"][0]["confidence"]
+            except:
+                ukrainian_text = ""
+                ukrainian_confidence = 0
+
+            if french_confidence == ukrainian_confidence and french_confidence == 0:
+                return "", None
+            if french_confidence >= ukrainian_confidence:
+                return french_text, "fr"
+            return ukrainian_text, "uk"
         except: 
             pass
 
-        return text_fr, text_ua
+        return 
 
 def convert_ogg_wav(file_path):
     sound = AudioSegment.from_ogg(file_path)
@@ -34,6 +85,14 @@ def delete_files(file_name):
     import_path = file_name.replace(".wav", ".ogg")
     os.remove(export_path)
     os.remove(import_path)
+
+def translate_voice_text(text, lang):
+    if lang == "uk":
+        translated_text = translateFunc(text, "fr", "uk")
+        return translated_text
+    translated_text = translateFunc(text, "uk", "fr")
+    return translated_text
+
 
 def translate_voice_texts(text_fr, text_ua):
     translated_text_ua = translateFunc(text_fr, "uk", "fr")
