e0df29d930cefad3c294e8d994bbf6516af85bd3
DobbiKov <dobbikov@gmail.com>
2022-08-25 21:57:04 +0200

    Added chat ids for translation

--- data.py
+++ data.py
@@ -2,4 +2,4 @@ from modules.quick_translate import quick_translate
 
 
 im_here = True
-quick_translate_chats = [-1001711218638, 716720991]
+quick_translate_chats = [-1001711218638, -640017880, 5200613842]
